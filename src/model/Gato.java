c589e1a45db20345cb221dfa68d93e76b9343329
Erick Thompson <95864907+erickthompson@users.noreply.github.com>
2022-02-15 17:41:08 -0300

    first commit

---
+++ src/model/Gato.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.util.Objects;
+
+public class Gato {
+    private string nome;
+    private string cor;
+    private string idade;
+
+    public Gato(string nome, string cor, string idade) {
+        this.nome = nome;
+        this.cor = cor;
+        this.idade = idade;
+    }
+
+    public string getNome() {
+        return nome;
+    }
+
+    public string getCor() {
+        return cor;
+    }
+
+    public string getIdade() {
+        return idade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Gato gato = (Gato) o;
+        return Objects.equals(nome, gato.nome) && Objects.equals(cor, gato.cor) && Objects.equals(idade, gato.idade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, cor, idade);
+    }
+
+    @Override
+    public String toString() {
+        return "Gato{" +
+                "nome=" + nome +
+                ", cor=" + cor +
+                ", idade=" + idade +
+                '}';
+    }
+}
